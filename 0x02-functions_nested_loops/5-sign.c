5f7e1df8dbcec5035edabeb33f9f0328128f1052
DennisKGyamfi <ngyamfi81@gmail.com>
2023-03-22 17:22:10 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,6 +7,7 @@
 * 0 and prints 0 if n is zero
 * -1 and prints - if n is less than zero
 */
+
 int print_sign(int n)
 {
 if (n > 0)
@@ -16,7 +17,7 @@ return (1);
 }
 else if (n == 0)
 {
-_putchar(0);
+_putchar('0');
 return (0);
 }
 else if (n < 0)
